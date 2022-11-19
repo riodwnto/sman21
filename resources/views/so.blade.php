88025adb463e30587a5e699fbe31554fc3ee51f9
riodwnto <riodwianto21@gmail.com>
2022-11-19 13:39:00 +0700

    .

--- resources/views/so.blade.php
+++ resources/views/so.blade.php
@@ -60,8 +60,8 @@
           <li><a class="nav-link scrollto " href="ppdb">PPDB</a></li>
           <li><a class="nav-link scrollto" href="#">Perpustakaan</a></li>
           <li><a class="nav-link scrollto" href="kontak">Kontak</a></li>
-          <li><a class="nav-link scrollto" href="#"><i class='bi bi-telephone-fill' style="font-size: 17px"></i></a></li>
-          <li><a class="nav-link scrollto" href="#"><i class='bi bi-envelope-fill' style="font-size: 17px"></i></a></li>
+          <li><a class="nav-link scrollto" href="tel:0227565909"><i class='bi bi-telephone-fill' style="font-size: 17px"></i></a></li>
+          <li><a class="nav-link scrollto" href="mailto:sman21bdg@yahoo.co.id" target="_blank""><i class='bi bi-envelope-fill' style="font-size: 17px"></i></a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
