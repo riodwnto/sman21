4ff7adfe462256a6638dec997467020278d4834b
arielfadhilah <ariel.deni47@gmail.com>
2023-05-09 00:19:52 +0700

    Update kontak.blade.php

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -17,15 +17,15 @@
     <div class="container" data-aos="fade-up">
 
       <div class="section-title">
-        <h2>Contact</h2>
-        <h3><span>Contact Us</span></h3>
+        <h2>Kontak</h2>
+        <h3><span>Kontak Kami</span></h3>
       </div>
 
       <div class="row" data-aos="fade-up" data-aos-delay="100">
         <div class="col-lg-6">
           <div class="info-box mb-4">
             <i class="bx bx-map"></i>
-            <h3>Our Address</h3>
+            <h3>Alamat Sekolah</h3>
             <p>Jl. Manjahlega No.30, Margasari, Kec. Buahbatu, Kota Bandung, Jawa Barat 40286</p>
           </div>
         </div>
@@ -33,7 +33,7 @@
         <div class="col-lg-3 col-md-6">
           <div class="info-box  mb-4">
             <i class="bx bx-envelope"></i>
-            <h3>Email Us</h3>
+            <h3>Email</h3>
             <p>sman21bdg@yahoo.co.id</p>
           </div>
         </div>
