19a47059cabdcfc5ab41467c3022f579c3d52588
Maleakhi Ekklesia <ekiputra234@gmail.com>
2023-05-08 22:21:47 +0700

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -5,9 +5,9 @@
             &copy; 2022 <strong><span>A2 Dev's</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-            <a href="#"><i class="bi bi-facebook"></i></a>
-            <a href="#"><i class="bi bi-youtube"></i></a>
-            <a href="#"><i class="bi bi-instagram"></i></a>
+            <a href="https://www.facebook.com/sman21bandung"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.youtube.com/@sman21bandung6"><i class="bi bi-youtube"></i></a>
+            <a href="https://www.instagram.com/sman_21_bandung/"><i class="bi bi-instagram"></i></a>
         </div>
     </div>
 </footer><!-- End Footer -->
